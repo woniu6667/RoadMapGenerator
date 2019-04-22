86ef5dc6f24c09113c5bf4a9ad4a769ae4d72b58
Brandon Burke <bcburke93@gmail.com>
2019-04-22 09:09:22 -0500

    done

--- roadGenerator.py
+++ roadGenerator.py
@@ -67,7 +67,8 @@ def finalConnection(graph):
 clustersize = int(sys.argv[1])
 Graphy = clusters(clustersize)
 finalgraph = finalConnection(Graphy)
-
+fh = open("roadmap.txt", "wb")
+nx.write_edgelist(finalgraph, fh)
 for node in finalgraph:
     for i in range(len(name)):
         if(name[i] in node):
@@ -77,5 +78,4 @@ for node in finalgraph:
 plt.subplot(121)
 options = { 'width':.09, 'font_size':2,'node_size':2, 'with_labels':True} 
 nx.draw_networkx(finalgraph, alpha=.7, node_color=colorlist, **options)
-#plt.show()
 plt.savefig("RoadMap.png", dpi=1250, bbox_inches='tight', format="PNG")
